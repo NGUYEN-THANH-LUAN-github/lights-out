337e785bb8612d6cfa725d86886f17c54fa3aa33
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-08-07 17:17:23 +0900

    Updates

---
+++ precache-manifest.a744f2ad7003589a0a8d64f215e5296a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eec62241f60a0200ce8",
+    "url": "/lights-out/static/css/main.6bce1650.chunk.css"
+  },
+  {
+    "revision": "5eec62241f60a0200ce8",
+    "url": "/lights-out/static/js/main.5eec6224.chunk.js"
+  },
+  {
+    "revision": "57ce5305efbc6a181115",
+    "url": "/lights-out/static/js/1.57ce5305.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "e8bd61330515a9486ca30de88f134481",
+    "url": "/lights-out/index.html"
+  }
+];
